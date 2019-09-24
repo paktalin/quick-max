a1562c1ac4830dc314f576c3e2853ac2dd4770a5
likorn <likorn@ttu.ee>
2019-09-24 19:35:30 +0300

    Progress bar color is animated

--- app/src/main/java/com/example/quickmax/MainActivity.kt
+++ app/src/main/java/com/example/quickmax/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.quickmax
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
 import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var answerSet: AnswerSet
-    private val timeToSolve = 4000
+    private val timeToSolve:Long = 4000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         answerSet = AnswerSet(3)
         setUpAnswerButtons()
         timer.start()
+        startProgressBarAnimation()
     }
 
     private fun setUpAnswerButtons() {
@@ -30,7 +34,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun processAnswer(correct: Boolean) {
         timer.cancel()
-        makeRadioButtonsUncheckable()
+        makeButtonsUncheckable()
 
         val responseFragment = ResponseFragment.newInstance().also {
                     f -> f.arguments = Bundle().also {
@@ -42,10 +46,22 @@ class MainActivity : AppCompatActivity() {
             .commitAllowingStateLoss()
     }
 
-    private fun makeRadioButtonsUncheckable() {
+    private fun makeButtonsUncheckable() {
         for (answer in answerSet) {
             findViewById<CardView>(answer.buttonId).isClickable = false
         }
+    }
+
+    private fun startProgressBarAnimation() {
+        val colorFrom = ContextCompat.getColor(this, R.color.gradient_dark)
+        val colorTo = ContextCompat.getColor(this, R.color.red)
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, colorTo)
+        colorAnimation.duration = timeToSolve
+        colorAnimation.addUpdateListener { animator ->
+            progress_bar.progressDrawable.setColorFilter(animator.animatedValue as Int,
+                android.graphics.PorterDuff.Mode.SRC_ATOP)
+        }
+        colorAnimation.start()
     }
 
     fun reload() {
@@ -54,17 +70,17 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private val timer = object : CountDownTimer(timeToSolve.toLong(), 100) {
+    private val timer = object : CountDownTimer(timeToSolve, 100) {
         override fun onTick(millisUntilFinished: Long) {
             val progress = ((timeToSolve - millisUntilFinished).toFloat() / timeToSolve) * 100
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                bar_time_left.setProgress(progress.toInt(), true)
+                progress_bar.setProgress(progress.toInt(), true)
             else
-                bar_time_left.progress = progress.toInt()
+                progress_bar.progress = progress.toInt()
         }
 
         override fun onFinish() {
-            makeRadioButtonsUncheckable()
+            makeButtonsUncheckable()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.main_layout, TimeIsOverFragment.newInstance(), "time_is_over")
