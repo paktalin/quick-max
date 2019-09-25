d6d9a2b4fd07d719b597b837bcd50f5ad641c410
likorn <likorn@ttu.ee>
2019-09-25 17:02:55 +0300

    Selected number of digits is stored in memory

--- app/src/main/java/com/example/quickmax/MainActivity.kt
+++ app/src/main/java/com/example/quickmax/MainActivity.kt
@@ -5,33 +5,47 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.card.MaterialCardView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private var secToSolve: Int = 4
+    private var numDigits: Int = 3
+    private lateinit var checkedCard: MaterialCardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        secToSolve = retrieveSecondsToSolve()
+        retrieveSharedPrefs()
         seek_bar.setMinStartValue(secToSolve.toFloat()).apply()
 
-        seek_bar.setOnSeekbarChangeListener {
-            n -> seek_bar_value.text = resources.getString(R.string.time_to_solve, n.toString())
+        seek_bar.setOnSeekbarChangeListener { n ->
+            seek_bar_value.text = resources.getString(R.string.time_to_solve, n.toString())
             secToSolve = n.toInt()
         }
 
         card_2_digits.setOnClickListener(cardOnClickListener)
         card_3_digits.setOnClickListener(cardOnClickListener)
         card_4_digits.setOnClickListener(cardOnClickListener)
+
+        btn_play.setOnClickListener(playClickListener)
     }
 
     private val cardOnClickListener = View.OnClickListener { card ->
-        val numDigits = getTextView(card).text.toString().toInt()
+        val cardM = card as MaterialCardView
+        if (!cardM.isChecked) {
+            cardM.isChecked = true
+            numDigits = getTextView(card).text.toString().toInt()
+            listOf(card_2_digits, card_3_digits, card_4_digits).forEach { c ->
+                if (c.id != cardM.id)
+                    c.isChecked = false
+            }
+        }
+    }
 
+    private val playClickListener = View.OnClickListener {
         saveSelectedValues()
-
         val intent = Intent(this, TaskActivity::class.java).also { i ->
             i.putExtra("num_digits", numDigits)
             i.putExtra("sec_to_solve", secToSolve)
@@ -45,8 +59,10 @@ class MainActivity: AppCompatActivity() {
         editor.apply()
     }
 
-    private fun retrieveSecondsToSolve(): Int {
+    private fun retrieveSharedPrefs() {
         val prefs = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
-        return prefs.getInt("sec_to_solve", 4)
+        secToSolve = prefs.getInt("sec_to_solve", 4)
+        checkedCard = findViewById(prefs.getInt("checked_num_id", R.id.card_3_digits))
+        checkedCard.isChecked = true
     }
 }
