df14831ae025c1f8abddd8710b98e527dffa074f
likorn <likorn@ttu.ee>
2019-09-25 13:40:43 +0300

    Selected values are saved in SharedPreferences

--- app/src/main/java/com/example/quickmax/MainActivity.kt
+++ app/src/main/java/com/example/quickmax/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.quickmax
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -10,14 +11,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
 
+    private var secToSolve: Int = 4
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        secToSolve = retrieveSecondsToSolve()
+        seek_bar.setMinStartValue(secToSolve.toFloat()).apply()
 
         seek_bar.setOnSeekbarChangeListener {
             n -> seek_bar_value.text = resources.getString(R.string.time_to_solve, n.toString())
+            secToSolve = n.toInt()
         }
-        seek_bar.setMinStartValue(4f).apply()
 
         card_2_digits.setOnClickListener(cardOnClickListener)
         card_3_digits.setOnClickListener(cardOnClickListener)
@@ -26,12 +31,24 @@ class MainActivity: AppCompatActivity() {
 
     private val cardOnClickListener = View.OnClickListener { card ->
         val numDigits = ((card as CardView).getChildAt(0) as TextView).text.toString().toInt()
-        val secToSolve = seek_bar.selectedMinValue.toInt()
+
+        saveSelectedValues()
 
         val intent = Intent(this, TaskActivity::class.java).also { i ->
             i.putExtra("num_digits", numDigits)
             i.putExtra("sec_to_solve", secToSolve)
         }
         startActivity(intent)
+    }
+
+    private fun saveSelectedValues() {
+        val editor = getSharedPreferences("my_prefs", Context.MODE_PRIVATE).edit()
+        editor.putInt("sec_to_solve", secToSolve)
+        editor.apply()
+    }
+
+    private fun retrieveSecondsToSolve(): Int {
+        val prefs = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        return prefs.getInt("sec_to_solve", 4)
     }
 }
