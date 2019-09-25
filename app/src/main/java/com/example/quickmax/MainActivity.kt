4ff3f1db5017c214ad5c3cb9c1f0cb41078b0b6e
likorn <likorn@ttu.ee>
2019-09-25 11:22:07 +0300

    Save CheckableCardView tries

--- app/src/main/java/com/example/quickmax/MainActivity.kt
+++ app/src/main/java/com/example/quickmax/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.quickmax
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity: AppCompatActivity() {
@@ -9,5 +10,12 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seek_bar.setOnSeekbarChangeListener {
+            n -> seek_bar_value.text = resources.getString(R.string.time_to_solve, n.toString())
+        }
+
+
+        seek_bar.setMinStartValue(4f).apply()
     }
 }
