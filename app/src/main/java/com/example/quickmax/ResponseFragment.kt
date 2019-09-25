4079ae8a769129d69ccadfcfb455f10efb457d22
likorn <likorn@ttu.ee>
2019-09-25 14:25:23 +0300

    Replaced ImageButton with MaterialButton next

--- app/src/main/java/com/example/quickmax/ResponseFragment.kt
+++ app/src/main/java/com/example/quickmax/ResponseFragment.kt
@@ -1,14 +1,15 @@
 package com.example.quickmax
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
 
 class ResponseFragment: Fragment() {
 
@@ -24,15 +25,25 @@ class ResponseFragment: Fragment() {
         val view = inflater.inflate(R.layout.fragment_response, container, false)
         val correct = arguments!!.getBoolean("correct")
         return if (correct)
-            view(view, ContextCompat.getColor(activity!!, R.color.colorAccent), R.string.response_correct)
+            view(view, color(R.color.colorAccent), R.string.response_correct, color(R.color.transparent_dark_black))
         else
-            view(view, ContextCompat.getColor(activity!!, R.color.colorPrimary), R.string.response_wrong)
+            view(view, color(R.color.colorPrimary), R.string.response_wrong, Color.WHITE)
     }
 
-    private fun view(view: View, color: Int, responseId: Int): View {
-        view.findViewById<TextView>(R.id.tv_response).text = resources.getString(responseId)
-        view.findViewById<ConstraintLayout>(R.id.response_layout).setBackgroundColor(color)
-        view.findViewById<ImageButton>(R.id.btn_next).setOnClickListener { (activity as TaskActivity).reload() }
+    private fun view(view: View, color: Int, responseId: Int, textColor: Int): View {
+        view.findViewById<TextView>(R.id.tv_response).apply {
+            text = resources.getString(responseId)
+            setTextColor(color)
+        }
+
+        view.findViewById<MaterialButton>(R.id.btn_next).apply {
+            background.setColorFilter(color, PorterDuff.Mode.MULTIPLY)
+            setTextColor(textColor)
+            setOnClickListener { (activity as TaskActivity).reload() } }
         return view
+    }
+
+    private fun color(colorId: Int): Int {
+        return ContextCompat.getColor(activity!!, colorId)
     }
 }
